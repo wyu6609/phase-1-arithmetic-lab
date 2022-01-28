ac47ada23f80b7db68cf9b50e0c22565798a0757
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-01-28 17:43:51 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 16;
+const num4 = 12;
+const mod = num3 % num4;
+
+const max = Math.max(13, 2, 8, 20, 11);
